145a9031c57e9bd51b5a793969cbe72fa2b700e4
Christoph Guttandin <chrisguttandin@media-codings.com>
2016-02-13 01:59:18 +0100

    configure karma for SauceLabs

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -1,34 +1,85 @@
 'use strict';
 
+var browserify = require('../../package.json').browserify;
+
 module.exports = function (config) {
 
-    config.set({
+    /* eslint-disable indent */
+    var configuration = {
 
-        basePath: '../../',
+            basePath: '../../',
 
-        browsers: [
+            files: [
+                'test/unit/**/*.js'
+            ],
+
+            frameworks: [
+                'browserify',
+                'mocha',
+                'sinon-chai' // implicitly uses chai too
+            ],
+
+            preprocessors: {
+                'test/unit/**/*.js': 'browserify'
+            }
+
+        };
+    /* eslint-enable indent */
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = [
+            'ChromeCanarySauceLabs',
+            'ChromeSauceLabs',
+            'FirefoxDeveloperSauceLabs',
+            'FirefoxSauceLabs',
+            'SafariSauceLabs'
+        ];
+
+        configuration.captureTimeout = 120000;
+
+        configuration.customLaunchers = {
+            ChromeCanarySauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'OS X 10.11',
+                version: 'dev'
+            },
+            ChromeSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'OS X 10.11'
+            },
+            FirefoxDeveloperSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'OS X 10.11',
+                version: 'dev'
+            },
+            FirefoxSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'OS X 10.11'
+            },
+            SafariSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'safari',
+                platform: 'OS X 10.11',
+                version: '9.0'
+            }
+        };
+
+        configuration.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    } else {
+        configuration.browsers = [
             'Chrome',
             'ChromeCanary',
             'Firefox',
             'FirefoxDeveloper',
             'Opera',
             'Safari'
-        ],
+        ];
+    }
 
-        files: [
-            'test/unit/**/*.js'
-        ],
-
-        frameworks: [
-            'browserify',
-            'mocha',
-            'sinon-chai' // implicitly uses chai too
-        ],
-
-        preprocessors: {
-            'test/unit/**/*.js': 'browserify'
-        }
-
-    });
+    config.set(configuration);
 
 };
