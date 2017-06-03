559de0aea150bc347f983d953295eded8a80faab
chrisguttandin <chrisguttandin@media-codings.com>
2017-06-03 13:30:51 +0200

    refactor AudioEventScheduler

--- src/helper/audio-event-scheduler.ts
+++ src/helper/audio-event-scheduler.ts
@@ -12,26 +12,22 @@ export class AudioEventScheduler {
     }
 
     public cancel (definition: IDefinition) {
-        this._definitions.some((d) => {
-            const found = (definition === d);
+        const index = this._definitions.indexOf(definition);
 
-            if (found) {
-                this._definitions.splice(this._definitions.indexOf(d), 1);
-            }
-
-            return found;
-        });
+        if (index > -1) {
+            this._definitions.splice(index, 1);
+        }
     }
 
     public flush (elapsedTime: number) {
         const currentTimeAfterwards = this.currentTime += elapsedTime;
 
-        while (this._definitions.length && this._definitions[0].when <= currentTimeAfterwards) {
+        while (this._definitions.length > 0 && this._definitions[0].when <= currentTimeAfterwards) {
             // TypeScript needs to be convinced that definition is not undefined.
-            const definition = <IDefinition> this._definitions.shift();
+            const { func, when } = <IDefinition> this._definitions.shift();
 
-            this.currentTime = definition.when;
-            definition.func();
+            this.currentTime = when;
+            func();
         }
 
         this.currentTime = currentTimeAfterwards;
@@ -39,7 +35,7 @@ export class AudioEventScheduler {
 
     public reset () {
         this.currentTime = 0;
-        this._definitions = [];
+        this._definitions.length = 0;
     }
 
     public schedule (definition: IDefinition) {
